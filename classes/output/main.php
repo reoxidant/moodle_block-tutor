a31c381366a61736f835eb3849a20c4e9a93f7a8
Vitaliy Shapovalov <xflash55@yandex.ru>
2020-11-19 17:22:39 +0300

    update #5-issue show only group with 50+- studends and if student more then 50 render button on the page

--- classes/output/main.php
+++ classes/output/main.php
@@ -96,7 +96,7 @@ class main implements renderable, templatable
                 'bystudent' => $studentlist_bystudent,
                 'bygroup' => $studentlist_bygroup
             ],
-            'show_students_by_action_button' => ($_POST['show_more_students']) ? false : true
+            'show_students_by_action_button' => $studentslistview->countStudents >= 50
         ];
     }
 }
