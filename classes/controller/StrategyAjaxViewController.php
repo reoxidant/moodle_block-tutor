0a6f7d2036ab4b00f633608e6b093a891b9b602d
Vitaliy Shapovalov <xflash55@yandex.ru>
2021-02-03 11:17:01 +0300

    refactor code in each function for resolve a problem with collection data for html template

--- classes/controller/StrategyAjaxViewController.php
+++ classes/controller/StrategyAjaxViewController.php
@@ -49,73 +49,106 @@ class StrategyAjaxViewController extends sirius_student implements Strategy
     {
         $this -> chosen_id = $chosen_id;
         $this -> select_list = $select_list;
-
     }
 
     /**
-     * @return array
+     * @return array|void
      * @throws dml_exception
      * @throws \moodle_exception
      */
     public function get_students(): array
     {
         $cache = \cache ::make('block_tutor', 'student_screen_data');
-        //TODO: here is only grouplist
-
-        if ($select_list = "") {
-
-        }
-        //TODO: here is only studentlist
         if ($this -> select_list === "grouplist") {
-            if ($studentsCache = $cache -> get('student_screen_data')["groups"]) {
-                $group_id = $this -> chosen_id;
-                $group = new group($group_id, null);
-                $group -> get_groups_data_from_cache($studentsCache);
-
-                foreach ($group -> students as $group_student) {
-                    $studentCourse = $cache -> get('student_screen_data')["students"][$group_student -> id];
-                    $student = new student($group_student -> id, null, null);
-                    $student -> check_student_hasfindebt();
-                    $student -> set_student_leangroup();
-
-                    foreach ($studentCourse["studentdata"] as $courseid => $course) {
-                        if (in_array($group_id, $course['groupid'])) {
-                            $student -> studentdata["course_data"][] = $course["course_data"];
-                            if ($grade_mod = $student -> set_mod_info($courseid, $group_id)) {
-                                $course["course_data"] -> mod_info[] = $grade_mod;
-                            }
-                        }
-                        continue;
-                    }
-                    $group -> students[$student -> studentid] = $student;
-                }
-                return (array)$group;
-            }
+            if ($studentsCache = $cache -> get('student_screen_data')["groups"]) return $this -> collectGroupTemplateData();
             return array();
         } else if ($this -> select_list === "studentlist") {
-            if ($studentsCache = $cache -> get('student_screen_data')["students"]) {
-                $student_id = $this -> chosen_id;
-                $student = new student($student_id, null, null);
-                $student -> get_student_data_from_cache($studentsCache);
-                $student -> check_student_hasfindebt();
-                $student -> set_student_leangroup();
+            if ($studentsCache = $cache -> get('student_screen_data')["students"]) return $this -> collectStudentTemplateData();
+            return array();
+        }
+    }
 
-                foreach ($student -> studentdata as $courseid => $course) {
-                    $course["course_data"] -> url = new moodle_url('/course/view.php', array('id' => $courseid));
-                    foreach ($course['groupid'] as $groupid) {
-                        if ($grade_mod = $student -> set_mod_info($courseid, $groupid)) {
-                            $course["course_data"] -> mod_info[] = $grade_mod;
-                        }
+    /**
+     * @return array
+     */
+    private function collectStudentTemplateData(): array
+    {
+        $student_id = $this -> chosen_id;
+        try {
+            $student = new student($student_id, null, null);
+        } catch (dml_exception | \moodle_exception $e) {
+            debugging($e -> getMessage());
+        }
+        $student -> get_student_data_from_cache($studentsCache);
+        $student -> check_student_hasfindebt();
+        try {
+            $student -> set_student_leangroup();
+        } catch (dml_exception $e) {
+            debugging($e -> getMessage());
+        }
+
+        foreach ($student -> studentdata as $courseid => $course) {
+            try {
+                $course["course_data"] -> url = new moodle_url('/course/view.php', array('id' => $courseid));
+            } catch (\moodle_exception $e) {
+                debugging($e -> getMessage());
+            }
+            foreach ($course['groupid'] as $groupid) {
+                try {
+                    if ($grade_mod = $student -> set_mod_info($courseid, $groupid)) {
+                        $course["course_data"] -> mod_info[] = $grade_mod;
                     }
+                } catch (dml_exception $e) {
+                    debugging($e -> getMessage());
                 }
-
-                usort($student -> studentdata, array('self', 'cmp'));
-
-                return (array)$student;
-            } else {
-                return array();
             }
         }
+
+        usort($student -> studentdata, array('self', 'cmp'));
+
+        return (array)$student;
+    }
+
+    /**
+     * @return array
+     */
+    private function collectGroupTemplateData(): array
+    {
+        $group_id = $this -> chosen_id;
+        $group = new group($group_id, null);
+        $group -> get_groups_data_from_cache($studentsCache);
+
+        foreach ($group -> students as $group_student) {
+            $studentCourse = $cache -> get('student_screen_data')["students"][$group_student -> id];
+            try {
+                $student = new student($group_student -> id, null, null);
+            } catch (dml_exception | \moodle_exception $e) {
+                debugging($e -> getMessage());
+            }
+            $student -> check_student_hasfindebt();
+            try {
+                $student -> set_student_leangroup();
+            } catch (dml_exception $e) {
+                debugging($e -> getMessage());
+            }
+
+            foreach ($studentCourse["studentdata"] as $courseid => $course) {
+                if (in_array($group_id, $course['groupid'])) {
+                    $student -> studentdata["course_data"][] = $course["course_data"];
+                    try {
+                        if ($grade_mod = $student -> set_mod_info($courseid, $group_id)) {
+                            $course["course_data"] -> mod_info[] = $grade_mod;
+                        }
+                    } catch (dml_exception $e) {
+                        debugging($e -> getMessage());
+                    }
+                }
+                continue;
+            }
+            $group -> students[$student -> studentid] = $student;
+        }
+
+        return (array)$group;
     }
 
     /**
